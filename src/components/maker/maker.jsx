c59796c9ec568790b2828e39c388d350b2d7e96b
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-03-03 07:56:12 +0900

    add card and edit from component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
+    const [cards, setCards] = useState([
+        {
+            id:'1',
+            name: 'Elin1',
+            company:'Samsung',
+            theme:'dark',
+            title:'Front-end',
+            email:'thx@gmail.com',
+            message:'go for it',
+            fileName:'elin',
+            fileURL: null
+        },
+        {
+            id:'2',
+            name: 'Elin2',
+            company:'Samsung',
+            theme:'light',
+            title:'Front-end',
+            email:'thx@gmail.com',
+            message:'go for it',
+            fileName:'ellie',
+            fileURL: 'elin.png'
+        },
+        {
+            id:'3',
+            name: 'Elin3',
+            company:'Samsung',
+            theme:'colorful',
+            title:'Front-end',
+            email:'thx@gmail.com',
+            message:'go for it',
+            fileName:'ellie',
+            fileURL: null
+        }
+    ])
     const history = useNavigate();
     const onLogout = ()=>{
         authService.logout();
@@ -23,8 +58,8 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards}/>
+                <Preview cards={cards}/>
             </div>
             <Footer />
         </section>
